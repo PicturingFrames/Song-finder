2d689f06456201d3442163d2b007019213485722
Pacpage <52772988+PicturingFrames@users.noreply.github.com>
2021-04-24 17:52:13 +0300

    Update script.js

--- Song finder/script.js
+++ Song finder/script.js
@@ -20,15 +20,42 @@ function showSongs(data) {
     let song = songs[i];
     songsHtml += `<div class="song">
         <div class="title">
-            <span>${song.title}</span>
-      </div>
+            <span>${song.artist.name}-</span><span>${song.title}</span>
+        </div>
+        <button id="lyrics-button" data-artist='${song.artist.name}' data-song='${song.title}'>Lyrics</button>
   </div>\n`
   }
 
   resultBlock.innerHTML = songsHtml;
 };
 
+async function showLyrics(e){
+  let lyrButton = e.target;
+  if (lyrButton.tagName === 'BUTTON'){
+    let artist = lyrButton.dataset.artist;
+    let song = lyrButton.dataset.song;
+    const response = await fetch(`${url}/v1/${artist}/${song}`)
+    const data = await response.json();
+    console.log(data)
+
+    if(data.error){
+      alert("lyrics not found");
+    } else {
+      resultBlock.innerHTML = data.lyrics;
+    }
+  }
+}
+
 searchButton.addEventListener('click', () => {
   const name = searchInput.value;
   search(name);
+});
+
+document.addEventListener('keydown', (e) => {
+  if (e.keyCode === 13) {
+    const title = searchInput.value;
+    search(title)
+  }
 })
+
+resultBlock.addEventListener('click', showLyrics)
